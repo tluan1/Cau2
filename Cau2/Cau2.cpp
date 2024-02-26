1d8bd6f64e34dc7729724effc03bb87bb7d127c7
ADMIN <ADMIN@LAB1-MAY14>
2024-02-26 08:33:02 +0700

    Add project files.

---
+++ Cau2/Cau2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, hieu;
+	cout << "CHUONG TRINH TINH HIEU\n";
+	cout << "nhap so thu nhat:"; cin >> a;
+	cout << "nhap so thu hai:"; cin >> b;
+	hieu = a - b;
+	cout << a << " -" << b << " =" << hieu << endl;
+	return 0;
+}
